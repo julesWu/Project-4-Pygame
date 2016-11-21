81475adfc5aa229c21b57b945aa2023e1a77ec2a
Julia Shuyue Wu <jules.wu@Julias-MacBook-Air.local>
2016-11-21 16:21:34 -0500

    Wrote part of cube and player class

--- pygame.source.code.py
+++ pygame.source.code.py
@@ -17,40 +17,107 @@ HEIGHT = 600
 BLACK     = (  0,   0,    0)
 GRAY 	  = (185, 185,  185)
 WHITE     = (255, 255,  255)
-YELLOW    = (155, 155,    0)
+YELLOW    = (255, 255,    0)
 RED       = (255,   0,    0)
 BLUE      = (  0,   0,  255)
 GREEN     = (  0, 255,    0)
-L_YELLOW  =	(155, 155,    0)
-L_GREEN   = ( 20,  175,  20)
-L_BLUE    = ( 20,   20, 175)
-L_RED     = (175,   20,  20)
-
+ORANGE    = (255, 140,    0)
+PURPLE    = (148,   0,  211)
 
 BACKG_COLOR1 = WHITE
 BACKG_COLOR2 = BLACK 
 
+
 class Cube(pygame.sprite.Sprite):
-	def _init_(self, color, width, height):
-
-		self.image = pygame.surface([width, height])
-
+	def __init__(self, color, width, height):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([width, height])
 		self.image.fill(color)
-
 		self.rect = self.image.get_rect()
+
+	def update(self):
+		self.rect.y +=3
+
+		if self.rect.y > 610:
+			self.rect.y = -25
+		
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, width, height):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([width,height])
+		self.image.fill(BLACK)
+		self.rect = self.image.get_rect()
+		self.rect.x = 400
+		self.rect.y = 550
+
+def random_color():
+	num = random.randint(1,5)
+	if num == 1:
+		return RED
+	elif num == 2:
+		return ORANGE
+	elif num == 3:
+		return PURPLE
+	elif num == 4:
+		return BLUE 
+	elif num == 5:
+		return YELLOW	
 
 pygame.init()
 
-Cube_list = pygame.sprite.Group()
+#set screen dimensions
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
+#A list to keep track of all the cubes
+cube_list = pygame.sprite.Group()
+
+#A list to keep track of all the different objects in the game.
 all_sprites_list = pygame.sprite.Group()
 
+#Actually creating the cubes
 for i in range(75):
-	cube = Cube(BLUE, 20, 20)
+	cube = Cube(random_color(), 20, 20)
 
-	cube.rect.x = random.randrange(WIDTH)
-	cube.rect.y = random.randrange(HEIGHT)
+	cube.rect.x = random.randrange(WIDTH - 20)
+	cube.rect.y = random.randrange(HEIGHT - 20)
 
-	Cube_list.add(cube)
+	cube_list.add(cube)
 	all_sprites_list.add(cube)
-	
+
+#Create a player
+player = Player(20, 20)
+all_sprites_list.add(player)
+
+#loop variable
+done = False
+
+clock = pygame.time.Clock()
+
+#-----MAIN--LOOP------------MAIN--LOOP----------MAIN--LOOP----------#
+
+while done == False:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+
+	#Clear the screen first		
+	screen.fill(WHITE)
+
+	cube_collide_list = pygame.sprite.spritecollide(player, cube_list, False)
+
+	#Draw all the sprites
+	all_sprites_list.draw(screen)
+
+	#moving the cubes down the screen
+	cube_list.update()
+
+	#Limit to 20 Frames per second
+	clock.tick(20)
+
+	pygame.display.flip()
+
+pygame.quit()
+
+
+
