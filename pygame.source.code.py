1a9e429e5a4a81e7ece1fd85fe5617a391015bf0
Julia Shuyue Wu <jules.wu@Julias-MacBook-Air.local>
2016-11-20 21:46:58 -0500

    First commit for pygame

---
+++ pygame.source.code.py
@@ -0,0 +1,32 @@
+#Julia Wu
+#UMID: 38632603
+#uniquename: shuyue
+#References:
+
+import random
+import pygame, sys
+import time
+from pygame.locals import *
+
+#Defining some constants for the game:
+W_WIDTH = 640
+W_HEIGT = 480
+B_WIDTH = 10
+B_HEIGHT = 20
+
+
+#Define Colors
+BLACK     = (  0,   0,    0)
+GRAY 	  = (185, 185,  185)
+WHITE     = (255, 255,  255)
+YELLOW    = (155, 155,    0)
+RED       = (255,   0,    0)
+BLUE      = (  0,   0,  255)
+GREEN     = (  0, 255,    0)
+L_YELLOW  =	(155, 155,    0)
+L_GREEN   = ( 20,  175,  20)
+L_BLUE    = ( 20,   20, 175)
+L_RED     = (175,   20,  20)
+
+
+BACKG_COLOR = BLACK
